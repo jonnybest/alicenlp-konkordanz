2c211832dab1903e3c06203f0c6003eea75ad4cd
jonnybest <jonathan.best@student.kit.edu>
2013-08-30 16:22:58 +0200

    extracting lexicographer file names from WordNet

--- src/main/java/test/Mainclass.java
+++ src/main/java/test/Mainclass.java
@@ -3,6 +3,11 @@
  */
 package test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 import edu.stanford.nlp.ling.CoreAnnotations.*;
@@ -27,11 +32,47 @@ public class Mainclass {
 		// Add your text here!
 		String text = "Frank told me about the first thing he learned in publishing: \"I was a young boy, when my supervisor took me aside,\" he "
 				+ "said. \"She told me to write fast and keep my mouth shut.\" "
-				+ "Frank shrugged and his face darkened.";	    
+				+ "Frank shrugged and his face darkened.";
+		
+		// texts go here:
+		List<String> texts = new LinkedList<String>();
 
 		// StanfordWebsiteExample.analyze(text);
 		
-		StaticDynamicClassifier.analyze(text);
+		/*
+		 * Load text from individual files
+		 */
+		File folder= new File("files/");
+		File[] listOfFiles = folder.listFiles();
+
+	    for (File item : listOfFiles) {
+	      if (item.isFile()) {
+	        /*
+	         * actual file loading 
+	         */
+	    	  try {
+				FileInputStream stream = new FileInputStream(item);
+				BufferedInputStream bufstr = new BufferedInputStream(stream);
+				bufstr.toString();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	    	  
+	      } else if (item.isDirectory()) {
+	        System.out.println("Skipped directory " + item.getName());
+	      }
+	    }
+		
+		
+		/* 
+		 * Run text through pipeline
+		 */
+		for (String item : texts) {
+			StaticDynamicClassifier.analyze(item);
+			System.out.println(text);
+		}
+		
+		//StaticDynamicClassifier.analyze(text);
 	    
 	    System.out.println("{End of runtime.}");
 	}
--- src/main/java/test/StaticDynamicClassifier.java
+++ src/main/java/test/StaticDynamicClassifier.java
@@ -1,13 +1,15 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-import net.sf.extjwnl.JWNL;
 import net.sf.extjwnl.JWNLException;
 import net.sf.extjwnl.data.IndexWord;
 import net.sf.extjwnl.data.POS;
 import net.sf.extjwnl.data.PointerType;
 import net.sf.extjwnl.data.PointerUtils;
+import net.sf.extjwnl.data.Synset;
 import net.sf.extjwnl.data.list.PointerTargetNodeList;
 import net.sf.extjwnl.data.list.PointerTargetTree;
 import net.sf.extjwnl.data.relationship.AsymmetricRelationship;
@@ -17,66 +19,116 @@ import net.sf.extjwnl.data.relationship.RelationshipList;
 import net.sf.extjwnl.dictionary.Dictionary;
  
 
+
+
+
+
 import java.io.FileInputStream;
-
-
 import java.io.FileNotFoundException;
 
-import net.sf.extjwnl.JWNL;
-import net.sf.extjwnl.JWNLException;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.util.CoreMap;
 
 public class StaticDynamicClassifier {
+	static private StaticDynamicClassifier myinstance = null;
+	static private StanfordCoreNLP mypipeline = null;
 
-	public static void analyze(String text) {
-		
-		/* do word net stuff
-		 * 
-		 */
-	    JWNL.initialize();
-	    String something = JWNL.resolveMessage("dog");
-	    
-	    String propsFile = "src/main/java/test/file_properties.xml";
-	    FileInputStream properties = null;
-		try {
-			properties = new FileInputStream(propsFile);
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-	    
-        try {
-            //old-style, static singletone instance
-            //JWNL.initialize(new FileInputStream(propsFile));
-            //Dictionary dictionary = Dictionary.getInstance();
-
-            //new style, instance dictionary
-			Dictionary dictionary = Dictionary.getInstance(properties);
-            new StaticDynamicClassifier(dictionary).go();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }      
-        
-	    System.out.println(something);
-	    
-	    /* do corenlp stuff
+	public static void analyze(String text) {	    
+	    /* parse text with corenlp
 	     * 
 	     */
-	    // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-	    Properties props = new Properties();
-	    // nimm das wsj-bidirectional model, weil das bessere ergebnisse liefert
-	    props.put("pos.model", "edu/stanford/nlp/models/pos-tagger/wsj-bidirectional/wsj-0-18-bidirectional-distsim.tagger");
-	    // konfiguriere pipeline
-	    props.put("annotators", "tokenize, ssplit, pos, lemma, parse");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);	    
+		StanfordCoreNLP pipeline;
+		if (mypipeline == null) {			
+		    // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
+		    Properties props = new Properties();
+		    // nimm das wsj-bidirectional model, weil das bessere ergebnisse liefert
+		    props.put("pos.model", "edu/stanford/nlp/models/pos-tagger/wsj-bidirectional/wsj-0-18-bidirectional-distsim.tagger");
+		    // konfiguriere pipeline
+		    props.put("annotators", "tokenize, ssplit, pos, lemma");
+		    pipeline = new StanfordCoreNLP(props);	    
+		    mypipeline = pipeline;
+		}
+		else {
+			pipeline = mypipeline;
+		}
 	    
 	    // create an empty Annotation just with the given text
 	    Annotation document = new Annotation(text);
 	    
 	    // run all Annotators on this text
 	    pipeline.annotate(document);
+	    System.out.println("{annotation is now done}");
+
+	    // get all distinct verbs as a list
+	    List<String> verblist = new ArrayList<String>();
+	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);	    
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				// this is the POS tag of the token
+				String pos = token.get(PartOfSpeechAnnotation.class);
+				// TODO: using verbs is probably a bad idea: use verb phrases instead? Or better yet: predicate (root) only?
+				if (pos.startsWith("VB")) {
+					verblist.add(token.get(LemmaAnnotation.class));
+				}
+			}
+		}
+	
+		System.out.println("{have verb list with " + verblist.size() + " entries}");
+		System.out.println(verblist);
+	    
+	    /* do word net stuff
+	     * 
+	     */
+	    // set up properties file
+	    String propsFile = "src/main/java/test/file_properties.xml";
+	    FileInputStream properties = null;
+	    try {
+	    	properties = new FileInputStream(propsFile);
+	    } catch (FileNotFoundException e1) {
+	    	e1.printStackTrace();
+	    }
+	    
+	    // create a dictionary and run the analytics
+	    try {
+	    	
+	    	// run
+	    	StaticDynamicClassifier myclassifier; //.go();
+			if (myinstance == null) {
+				//new style, instance dictionary
+				Dictionary dictionary = Dictionary.getInstance(properties);
+				myclassifier = new StaticDynamicClassifier(dictionary);
+				myinstance = myclassifier;
+			}
+			else {
+				myclassifier = myinstance;
+			}
+	    	for (String token : verblist) 
+	    	{
+				myclassifier.analyzeLexicographerFileNamesForVerbs(token);
+			}
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    	System.exit(-1);
+	    }      
+	    /* analyse lexnames with wordnet
+	     * 
+	     */
 	}
 	
+	private void analyzeLexicographerFileNamesForVerbs(String token) throws JWNLException {
+		IndexWord word = dictionary.getIndexWord(POS.VERB, token);
+		demonstrateLexicographerFileNames(word);		
+	}
+
 	private IndexWord ACCOMPLISH;
     private IndexWord DOG;
     private IndexWord CAT;
@@ -95,6 +147,7 @@ public class StaticDynamicClassifier {
     }
 
     public void go() throws JWNLException, CloneNotSupportedException {
+    	demonstrateLexicographerFileNames(DOG);
         demonstrateMorphologicalAnalysis(MORPH_PHRASE);
         demonstrateListOperation(ACCOMPLISH);
         demonstrateTreeOperation(DOG);
@@ -102,7 +155,20 @@ public class StaticDynamicClassifier {
         demonstrateSymmetricRelationshipOperation(FUNNY, DROLL);
     }
 
-    private void demonstrateMorphologicalAnalysis(String phrase) throws JWNLException {
+	private void demonstrateLexicographerFileNames(IndexWord word) {
+		word.sortSenses();
+		List<Synset> senses = word.getSenses();
+		System.out.println("The word " + word + " is filed under the following lexicographer file names: ");
+		for (Synset synset : senses) {
+			if (senses.indexOf(synset) > 2) {
+				break;
+			}
+			System.out.print(synset.getLexFileName() + "[" + synset.getLexFileNum() + "] (" + word.getSynsetOffsets());
+		}
+		System.out.println();
+	}
+
+	private void demonstrateMorphologicalAnalysis(String phrase) throws JWNLException {
         // "running-away" is kind of a hard case because it involves
         // two words that are joined by a hyphen, and one of the words
         // is not stemmed. So we have to both remove the hyphen and stem
